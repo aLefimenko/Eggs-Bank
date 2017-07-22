08f4cb7c4a321c2358b58348bcbcbb111e02ab0a
Efimenko Alexander <sashok494949@yandex.ua>
2017-07-23 00:13:34 +0300

    0.0.3

---
+++ Assets/Scripts/EggTap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EggTap : MonoBehaviour {
+
+    private Animation animEgg;
+    private Text textScore;
+    private int i = 0;
+
+	void Start () {
+        textScore = GameObject.FindGameObjectWithTag("score").GetComponent<Text>();
+        animEgg = GetComponent<Animation>();
+        animEgg.Stop();
+        textScore.text = "Eggs : " + i.ToString();
+	}
+	
+
+	
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        i++;
+        if(animEgg.isPlaying)
+        {
+            animEgg.Stop();
+        }
+        animEgg.Play();
+        textScore.text = "Eggs : " + i.ToString();
+    }
+}
