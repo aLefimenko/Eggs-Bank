221b66b54f6ef428bf46c5a65326c15d5450ebb3
Efimenko Alexander <sashok494949@yandex.ua>
2017-07-23 11:57:42 +0300

    0.0.4

---
+++ Assets/Scripts/Objects/Click.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Click : Object, IUpgrade
+{
+    public void SellObject()
+    {
+        
+    }
+
+    public float UpgradeObject()
+    {
+        return Mathf.Sqrt
+    }
+
+    void Start()
+    {
+        type = "click";
+        level = 0;
+        value = Mathf.Pow(10f, level * 2.5f);
+        eggsPerSecond = UpgradeObject();
+    }
+
+  
+}
---
+++ Assets/Scripts/Objects/Object.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public abstract class Object
+{
+    protected string type;
+    protected int level;
+    protected float value;
+    protected float eggsPerSecond;
+    protected int upgrade;
+
+}
